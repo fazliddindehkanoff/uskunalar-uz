e4c88347309d3ef0ab1a53c508e74bd10b6e9c0d
DehqonovFazliddin <fazliddindehkanoff@yandex.com>
2024-03-28 13:23:22 +0500

    cors headers have been changed

--- config/settings.py
+++ config/settings.py
@@ -65,6 +65,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "https://management.rahimovschool.uz",
     "https://lms.rahimovschool.uz",
 ]
