49abc17ffde87a675d1d989e40fe03da5e3c5c53
DehqonovFazliddin <fazliddindehkanoff@yandex.com>
2024-04-04 10:32:11 +0500

    signals translation logic has been updated

--- api/models/signals.py
+++ api/models/signals.py
@@ -13,19 +13,23 @@ def translate_blank_fields(sender, instance, **kwargs):
     if instance._state.adding:
         translator = Translator()
         for field_name in instance.translated_fields:
-            value = getattr(instance, field_name)
-            if not value:  # Check if the field is blank
-                # Translate the corresponding filled field to the blank one
-                source_lang = "en"  # Assuming the source language is English
-                dest_lang = field_name.split("_")[
-                    -1
-                ]  # Extract language code from field name
-                filled_field_name = field_name.replace(
-                    f"_{dest_lang}", f"_{source_lang}"
-                )
-                filled_value = getattr(instance, filled_field_name)
-                if filled_value:
-                    translated_value = translator.translate(
-                        filled_value, src=source_lang, dest=dest_lang
-                    ).text
-                    setattr(instance, field_name, translated_value)
+            print(field_name)
+            try:
+                value = getattr(instance, field_name)
+                if not value:  # Check if the field is blank
+                    # Translate the corresponding filled field to the blank one
+                    source_lang = "en"  # Assuming the source language is English
+                    dest_lang = field_name.split("_")[
+                        -1
+                    ]  # Extract language code from field name
+                    filled_field_name = field_name.replace(
+                        f"_{dest_lang}", f"_{source_lang}"
+                    )
+                    filled_value = getattr(instance, filled_field_name)
+                    if filled_value:
+                        translated_value = translator.translate(
+                            filled_value, src=source_lang, dest=dest_lang
+                        ).text
+                        setattr(instance, field_name, translated_value)
+            except Exception:
+                pass
