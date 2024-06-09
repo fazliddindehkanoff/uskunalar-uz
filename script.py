92301aee323ecb569c8f4b4ddc8347ec049c2f6f
FazliddinDehkanoff <fazliddindehkanoff@yandex.com>
2024-06-10 03:55:48 +0500

    new function has been added

--- script.py
+++ script.py
@@ -4,9 +4,23 @@ import markdownify
 from api.models import Product
 
 
+def handle_images(soup):
+    """
+    Convert <img> tags into Markdown image format.
+    """
+    for img in soup.find_all("img"):
+        alt_text = img.get("alt", "")
+        src = img.get("src", "")
+        markdown_image = f"![{alt_text}]({src})"
+        img.replace_with(markdown_image)
+
+
 def html_to_markdown(html_content):
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(html_content, "html.parser")
+
+    # Handle images
+    handle_images(soup)
 
     # Convert the parsed HTML content to Markdown format
     markdown_content = markdownify.markdownify(str(soup), heading_style="ATX")
