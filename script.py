11dd144755a7095aace294a75645a10001934be7
FazliddinDehkanoff <fazliddindehkanoff@yandex.com>
2024-06-10 03:33:09 +0500

    new function has been added

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import markdownify
 
+from api.models import Product
+
 
 def html_to_markdown(html_content):
     # Parse the HTML content using BeautifulSoup
@@ -10,3 +12,12 @@ def html_to_markdown(html_content):
     markdown_content = markdownify.markdownify(str(soup), heading_style="ATX")
 
     return markdown_content
+
+
+def convert_html_to_markdown():
+    products = Product.objects.all()
+    for product in products:
+        product.description_uz = html_to_markdown(product.description_uz)
+        product.description_ru = html_to_markdown(product.description_ru)
+        product.description_en = html_to_markdown(product.description_en)
+        product.save()
