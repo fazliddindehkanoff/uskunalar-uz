7113eea8272717fc0f5d1d15fc9a541ca57f8d5f
FazliddinDehkanoff <fazliddindehkanoff@yandex.com>
2024-06-10 04:53:50 +0500

    bug fix

--- script.py
+++ script.py
@@ -2,39 +2,13 @@ import html2text
 from api.models import Product
 
 
-def handle_images(html_content):
-    """
-    Convert <img> tags into Markdown image format.
-    """
-    h = html2text.HTML2Text()
-    h.ignore_images = False
-    h.ignore_links = False
-    h.images_as_html = False
-    return h.handle(html_content)
-
-
-def parse_table(html_content):
-    """
-    Convert an HTML table to Markdown format while preserving the structure.
-    """
-    h = html2text.HTML2Text()
-    h.ignore_tables = False
-    return h.handle(html_content)
-
-
 def html_to_markdown(html_content):
-    # Convert images
-    html_content = handle_images(html_content)
-
-    # Convert tables
-    html_content = parse_table(html_content)
 
     # Convert the remaining HTML content to Markdown format
     h = html2text.HTML2Text()
-    h.ignore_images = True
-    h.ignore_tables = True
+    h.ignore_images = False
+    h.ignore_tables = False
     h.ignore_links = False
-    h.body_width = 0
     markdown_content = h.handle(html_content)
 
     return markdown_content
