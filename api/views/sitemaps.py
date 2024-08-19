3f37c4b3627831d90cbe7aff5fdc47431ebcca6d
Fazliddin <fazliddindehkanoff@yandex.com>
2024-08-19 11:03:52 +0500

    last changes

--- api/views/sitemaps.py
+++ api/views/sitemaps.py
@@ -6,20 +6,32 @@ from api.utils import get_list_of_object_ids
 
 
 class ProductSitemapApiView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
     def get(self, request):
         return Response(get_list_of_object_ids(Product, approved=True))
 
 
 class LineSitemapApiView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
     def get(self, request):
         return Response(get_list_of_object_ids(Line))
 
 
 class WorkSitemapApiView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
     def get(self, request):
         return Response(get_list_of_object_ids(Work))
 
 
 class VideoSitemapApiView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
     def get(self, request):
         return Response(get_list_of_object_ids(Video))
