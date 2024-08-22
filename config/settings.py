d2763d793cc1d95181e4c0b018f2b16e71357679
Fazliddin <fazliddindehkanoff@yandex.com>
2024-08-22 14:52:08 +0500

    bug fix

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://uskunalar.uz",
+    "https://uskunalar.uz/",
     "https://www.uskunalar.uz",
+    "https://www.uskunalar.uz/",
 ]
 
 CORS_ALLOW_HEADERS = [
