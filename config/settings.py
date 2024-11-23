679f6118f5c5dbd5072e0baf1511df1da1a0fd98
Fazliddin <fazliddindehkanoff@yandex.com>
2024-11-23 17:15:34 +0500

    new origin allowed

--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://uskunalar.uz",
     "https://www.uskunalar.uz",
+    "https://uskunalaruz.netlify.app",
 ]
 
 CORS_ALLOW_HEADERS = [
